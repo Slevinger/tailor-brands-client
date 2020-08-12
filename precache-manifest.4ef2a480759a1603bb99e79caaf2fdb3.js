972a800a60174f0720cef5cbe97de8c7b474df53
slevinger <shir.levinger@gmail.com>
2020-08-12 16:35:41 +0300

    Updates

---
+++ precache-manifest.4ef2a480759a1603bb99e79caaf2fdb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd5308250fe53a26b8a35512ffcf8eb",
+    "url": "/tailor-brands-client/index.html"
+  },
+  {
+    "revision": "3043baf0d511c2817c07",
+    "url": "/tailor-brands-client/static/css/main.ed9e59ea.chunk.css"
+  },
+  {
+    "revision": "888be8c47d4c75dca53a",
+    "url": "/tailor-brands-client/static/js/2.f647c8fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tailor-brands-client/static/js/2.f647c8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3043baf0d511c2817c07",
+    "url": "/tailor-brands-client/static/js/main.609a2a46.chunk.js"
+  },
+  {
+    "revision": "35a4193c005abf98ee0f",
+    "url": "/tailor-brands-client/static/js/runtime-main.12592743.js"
+  }
+]);
